26041ed535d8783f0e7d647f4ee81686e6c1ec3a
moeltae <124945094+moeltae@users.noreply.github.com>
2024-11-14 10:17:27 -0800

    inventory search bar update page

--- controller/components/inventory/InventorySearch.tsx
+++ controller/components/inventory/InventorySearch.tsx
@@ -1,5 +1,18 @@
 import React from "react";
-import { FormControl, InputGroup, Input, InputLeftElement, InputRightElement, Box, List, ListItem, Text, Tooltip, VStack } from "@chakra-ui/react";
+import { 
+  FormControl, 
+  InputGroup, 
+  Input, 
+  InputLeftElement, 
+  InputRightElement, 
+  Box, 
+  List, 
+  ListItem, 
+  Text, 
+  Tooltip, 
+  VStack,
+  HStack
+} from "@chakra-ui/react";
 import { SearchIcon, CloseIcon } from "@chakra-ui/icons";
 import { Plate, Reagent } from "@/server/utils/InventoryClient";
 
@@ -35,8 +48,8 @@ const InventorySearch: React.FC<InventorySearchProps> = ({
   };
 
   return (
-    <VStack spacing={2} align="start" width="100%">
-      <FormControl>
+    <VStack spacing={2} align="stretch" width="100%">
+      <HStack spacing={4} width="100%">
         <InputGroup>
           <InputLeftElement pointerEvents="none">
             <SearchIcon color="gray.300" />
@@ -55,38 +68,41 @@ const InventorySearch: React.FC<InventorySearchProps> = ({
             />
           </InputRightElement>
         </InputGroup>
-      </FormControl>
-      <Box maxH="200px" overflowY="auto" width="100%">
-        <List spacing={2}>
-          {searchResults.map((result, index) => (
-            <ListItem
-              key={`${result.id}-${result.name}`}
-              bg={isDarkMode
-                ? index % 2 === 0 ? "gray.800" : "gray.700"
-                : index % 2 === 0 ? "white" : "gray.100"
-              }>
-              {isPlate(result) && (
-                <Tooltip label="Click to find corresponding plate">
-                  <Text onClick={() => onPlateSelect(result)} cursor="pointer">
-                    Plate: {result.name} | {result.plate_type} | {result.barcode} |{" "}
-                    {result.nest_id ? "Checked in" : "Not checked in"}
-                  </Text>
-                </Tooltip>
-              )}
-              {isReagent(result) && (
-                <Tooltip label="Click to find corresponding plate">
-                  <Text onClick={() => onReagentSelect(result)} cursor="pointer">
-                    {isNumber(result.name)
-                      ? `Culture: ${result.name} | creation: ${result.expiration_date} | `
-                      : `Reagent: ${result.name} | ${result.volume} µL | expiry: ${result.expiration_date} | `}
-                    inventory well ID {result.well_id}
-                  </Text>
-                </Tooltip>
-              )}
-            </ListItem>
-          ))}
-        </List>
-      </Box>
+      </HStack>
+
+      {searchResults.length > 0 && (
+        <Box maxH="200px" overflowY="auto" width="100%">
+          <List spacing={2}>
+            {searchResults.map((result, index) => (
+              <ListItem
+                key={`${result.id}-${result.name}`}
+                bg={isDarkMode
+                  ? index % 2 === 0 ? "gray.800" : "gray.700"
+                  : index % 2 === 0 ? "white" : "gray.100"
+                }>
+                {isPlate(result) && (
+                  <Tooltip label="Click to find corresponding plate">
+                    <Text onClick={() => onPlateSelect(result)} cursor="pointer">
+                      Plate: {result.name} | {result.plate_type} | {result.barcode} |{" "}
+                      {result.nest_id ? "Checked in" : "Not checked in"}
+                    </Text>
+                  </Tooltip>
+                )}
+                {isReagent(result) && (
+                  <Tooltip label="Click to find corresponding plate">
+                    <Text onClick={() => onReagentSelect(result)} cursor="pointer">
+                      {isNumber(result.name)
+                        ? `Culture: ${result.name} | creation: ${result.expiration_date} | `
+                        : `Reagent: ${result.name} | ${result.volume} µL | expiry: ${result.expiration_date} | `}
+                      inventory well ID {result.well_id}
+                    </Text>
+                  </Tooltip>
+                )}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      )}
     </VStack>
   );
 };
