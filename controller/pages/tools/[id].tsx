eb4457d52cdfb34ef5c2075cbd212109544f5626
moeltae <124945094+moeltae@users.noreply.github.com>
2024-12-10 22:18:25 -0800

    disabling teach pendant related files for now

--- controller/pages/tools/[id].tsx
+++ controller/pages/tools/[id].tsx
@@ -23,7 +23,7 @@ import { ToolType } from "gen-interfaces/controller";
 import { capitalizeFirst } from "@/utils/parser";
 import { useParams } from "react-router";
 import Head from "next/head";
-import { TeachPendant } from "@/components/tools/advanced/teach_pendant/TeachPendant";
+// import { TeachPendant } from "@/components/tools/advanced/teach_pendant/TeachPendant";
 // Assuming you're using TypeScript, you could define a type for the status object
 type CommandStatus = {
   [commandName: string]: "idle" | "success" | "error";
@@ -692,9 +692,7 @@ export default function Page() {
 
           {/* Right Side */}
           {config?.type === ToolType.pf400 && (
-            <Box flex={1}>
-              <TeachPendant toolId={id || ""} config={config} />
-            </Box>
+            <Box flex={1}>{/* <TeachPendant toolId={id || ""} config={config} /> */}</Box>
           )}
         </HStack>
       </Box>
