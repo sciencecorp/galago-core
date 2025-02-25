972d9a9e5195302b1bfd4a1f81f72e57a330491a
Silvio Ortiz <Silvio Ortiz>
2025-02-25 11:00:29 -0800

    tool box cant be disabled

--- controller/components/tools/ToolConfigEditor.tsx
+++ controller/components/tools/ToolConfigEditor.tsx
@@ -58,7 +58,7 @@ export function ToolConfigEditor({
     statusQuery.isSuccess &&
     statusQuery.data &&
     statusQuery.data.status !== ToolStatus.OFFLINE &&
-    toolId != "Tool Box";
+    toolId != "tool_box";
   const toolType = defaultConfig.type;
   const config = toolSpecificConfig(defaultConfig);
   const [configString, setConfigString] = useState(JSON.stringify(config, null, 2));
