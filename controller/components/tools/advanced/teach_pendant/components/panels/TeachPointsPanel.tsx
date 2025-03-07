f90bb5fcb49563801d7b59b5edd28d8069b86778
moeltae <124945094+moeltae@users.noreply.github.com>
2025-03-06 16:52:23 -0800

    color fix

--- controller/components/tools/advanced/teach_pendant/components/panels/TeachPointsPanel.tsx
+++ controller/components/tools/advanced/teach_pendant/components/panels/TeachPointsPanel.tsx
@@ -228,7 +228,7 @@ export const TeachPointsPanel: React.FC<TeachPointsPanelProps> = ({
                           size="sm"
                           borderColor={borderColor}
                         />
-                        <MenuList borderColor={borderColor} bg={tableBgColor}>
+                        <MenuList >
                           <MenuItem icon={<FaPlay />} onClick={() => onMove(point)}>
                             Move to point
                           </MenuItem>
