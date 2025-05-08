e8b07d92c2123907a54500a66518500cb66f8c1c
Silvio Ortiz <silvioo@science.xyz>
2025-05-08 12:44:10 -0700

    fix prettier

--- controller/components/tools/advanced/teach_pendant/hooks/useTeachPendantData.ts
+++ controller/components/tools/advanced/teach_pendant/hooks/useTeachPendantData.ts
@@ -26,9 +26,7 @@ export const useTeachPendantData = () => {
   };
 
   const updateMotionProfile = (profile: MotionProfile) => {
-    setMotionProfiles((prev) =>
-      prev.map((p) => (p.id === profile.id ? profile : p)),
-    );
+    setMotionProfiles((prev) => prev.map((p) => (p.id === profile.id ? profile : p)));
   };
 
   const deleteMotionProfile = (id: number) => {
