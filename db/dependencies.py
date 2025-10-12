b7aa0910ff531b61b946fb19b067d2c2f06f9ded
Silvio Ortiz <silviortiz93@gmail.com>
2025-10-11 22:05:23 -0700

    add nests and scripts routerts

--- db/dependencies.py
+++ db/dependencies.py
@@ -1,6 +1,8 @@
+import typing as t
+from fastapi import HTTPException, Depends
 from sqlalchemy.orm import Session
 from db.models.db_session import SessionLocal, LogsSessionLocal
-import typing as t
+from db import crud
 
 
 def get_db() -> t.Generator[Session, None, None]:
@@ -17,3 +19,43 @@ def log_db() -> t.Generator[Session, None, None]:
         yield db_session
     finally:
         db_session.close()
+
+
+def get_selected_workcell_name(db: Session) -> str:
+    """Get the currently selected workcell name from settings.
+    
+    Raises:
+        HTTPException: If no workcell is selected
+    """
+    workcell_setting = crud.settings.get_by(db, obj_in={"name": "workcell"})
+    if not workcell_setting or not workcell_setting.is_active:
+        raise HTTPException(
+            status_code=400, 
+            detail="No workcell is currently selected. Please select a workcell in settings."
+        )
+    return workcell_setting.value
+
+
+def get_selected_workcell_id(db: Session) -> int:
+    """Get the currently selected workcell ID from settings.
+    
+    Raises:
+        HTTPException: If no workcell is selected or workcell doesn't exist
+    """
+    # Get the workcell setting
+    workcell_setting = crud.settings.get_by(db, obj_in={"name": "workcell"})
+    if not workcell_setting or not workcell_setting.is_active:
+        raise HTTPException(
+            status_code=400, 
+            detail="No workcell is currently selected. Please select a workcell in settings."
+        )
+    
+    # Get the workcell by name
+    selected_workcell = crud.workcell.get_by(db, obj_in={"name": workcell_setting.value})
+    if not selected_workcell:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Selected workcell '{workcell_setting.value}' not found"
+        )
+    
+    return selected_workcell.id
