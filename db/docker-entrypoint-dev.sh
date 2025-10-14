d28b1da1a96bcb29f600b9ec71c61145ed892ebb
Silvio Ortiz <150743006+silvioaburto@users.noreply.github.com>
2025-10-14 15:02:56 -0700

    upgrade python image (#140)
    * upgrade python image
    
    * fix reload issue introduce to dev container
    
    * revert docker compose prod
    
    * update workflow python dep

--- db/docker-entrypoint-dev.sh
+++ db/docker-entrypoint-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+export PYTHONPATH="/app"
 
-export PYTHONPATH=..
 if [ "$DEBUG" = "1" ]; then
     echo "Starting FastAPI application in debug mode..."
     python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m uvicorn db.api:app --host 0.0.0.0 --port 8000 --reload
