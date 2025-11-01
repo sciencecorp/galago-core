81f95ff9b16eb93e872faea4cf6e6c3a8d9d1d97
Silvio Ortiz <silviortiz93@gmail.com>
2025-11-01 09:44:40 -0700

    update config

--- db/inventory_config.py
+++ db/inventory_config.py
@@ -3,8 +3,20 @@ from typing import Dict, Tuple
 
 # Format: tool_type -> (rows, columns)
 INVENTORY_TOOL_MAP: Dict[str, Tuple[int, int]] = {
+    "alps": (1, 1),
+    "bioshake": (1, 1),
     "bravo": (3, 3),
-    "opentrons2": (3, 4),
+    "cytation": (1,1),
+    "hamilton": (5, 11),
+    "hig_centrifuge": (2, 1),
+    "liconic": (10, 5),
+    "microserve": (50, 14), 
+    "opentrons2": (4, 3),
+    "plateloc": (1, 1),
+    "spectramax": (1, 1),
+    "vcode": (1, 1),
+    "vprep": (3, 2),
+    "xpeel": (1, 1),
 }
 
 def get_inventory_config(tool_type: str) -> Tuple[int, int] | None:
