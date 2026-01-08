a0e3285e0df45fa4b0b32768d76c82ebe57bbecf
Silvio Ortiz <silvioo@science.xyz>
2026-01-07 22:24:59 -0800

    tutorial modal working

--- controller/pages/_app.tsx
+++ controller/pages/_app.tsx
@@ -1,22 +1,30 @@
+// pages/_app.tsx
 import "@/styles/globals.css";
 import { ChakraProvider, VStack, Box } from "@chakra-ui/react";
 import { trpc } from "../utils/trpc";
 import type { AppType } from "next/app";
 import Sidebar from "@/components/ui/SideBar";
 import { GlobalQueueStatusIndicator } from "@/components/runs/status/GlobalQueueStatusIndicator";
+import { TutorialProvider } from "@/components/tutorial/TutorialContext";
+import { tutorialSteps } from "@/components/tutorial/tutorialSteps";
+import { TutorialModal } from "@/components/tutorial/TutorialModal";
+
 require("log-timestamp");
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ChakraProvider>
-      <Sidebar>
-        <Box height="100vh" overflow="auto" display="flex" flexDirection="column">
-          <GlobalQueueStatusIndicator />
-          <VStack align="stretch" flex="1" overflow="auto" spacing={4} p={4}>
-            <Component {...pageProps} />
-          </VStack>
-        </Box>
-      </Sidebar>
+      <TutorialProvider steps={tutorialSteps}>
+        <Sidebar>
+          <Box height="100vh" overflow="auto" display="flex" flexDirection="column">
+            <GlobalQueueStatusIndicator />
+            <VStack align="stretch" flex="1" overflow="auto" spacing={4} p={4}>
+              <Component {...pageProps} />
+            </VStack>
+          </Box>
+        </Sidebar>
+        <TutorialModal />
+      </TutorialProvider>
     </ChakraProvider>
   );
 };
