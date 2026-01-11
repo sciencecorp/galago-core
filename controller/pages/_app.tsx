4af3ee77d61f6bc97b15d1f8561395ad38fc1a35
Silvio Ortiz <150743006+silvioaburto@users.noreply.github.com>
2026-01-11 10:16:13 -0800

    Autoscroll y global rm (#189)
    * rm height auto scroll for global view, should be handled by browser/desktop window
    
    * rm hits counter

--- controller/pages/_app.tsx
+++ controller/pages/_app.tsx
@@ -1,6 +1,5 @@
-// pages/_app.tsx
 import "@/styles/globals.css";
-import { ChakraProvider, VStack, Box } from "@chakra-ui/react";
+import { ChakraProvider, VStack } from "@chakra-ui/react";
 import { trpc } from "../utils/trpc";
 import type { AppType } from "next/app";
 import Sidebar from "@/components/ui/SideBar";
@@ -16,12 +15,10 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     <ChakraProvider>
       <TutorialProvider steps={tutorialSteps}>
         <Sidebar>
-          <Box height="100vh" overflow="auto" display="flex" flexDirection="column">
-            <GlobalQueueStatusIndicator />
-            <VStack align="stretch" flex="1" overflow="auto" spacing={4} p={4}>
-              <Component {...pageProps} />
-            </VStack>
-          </Box>
+          <GlobalQueueStatusIndicator />
+          <VStack align="stretch" flex="1" spacing={4} p={0}>
+            <Component {...pageProps} />
+          </VStack>
         </Sidebar>
         <TutorialModal />
       </TutorialProvider>
